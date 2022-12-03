fc4518584b156efcfffe1d3df987902c7af9293f
Safia Ahmedou <safia.ahmedou@gmail.com>
2022-12-03 21:09:55 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(
+    name='geocluster',
+    version='1.0',
+    author='Safia Ahmedou',
+    description='clusteting spatial data',
+    url='https://github.com/Safiaaaaa/geocluster',
+    keywords='spacial data, clustering, mapping, social data',
+    python_requires='>=3.7, <4',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires= requirements,
+    # package_data={
+    #     'sample': ['sample_data.csv'],
+    # },
+    # entry_points={
+    #     'runners': [
+    #         'sample=sample:main',
+    #     ]
+    # }
+)
