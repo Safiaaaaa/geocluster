61184e402bc851307dcfc02f96224dff145f2def
Safia Ahmedou <safia.ahmedou@gmail.com>
2022-12-08 16:23:58 +0100

    added dataframe, added hover data

--- geocluster/app.py
+++ geocluster/app.py
@@ -1,6 +1,4 @@
-import streamlit as st
-from streamlit_folium import folium_static
-import folium
+import streamlit as sts
 import pandas as pd
 import numpy as np
 import streamlit as st
@@ -10,22 +8,25 @@ import geopandas as gpd
 import plotly.express as px
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import minmax_scale
-# from PIL import Image
-
 # from geocluster.registry import feat_labels, feat_dict
 # from geocluster.kmeans import cluster
 
+# Loading data - berlin geojson and features dataframe
+
+geo = gpd.read_file('https://raw.githubusercontent.com/Safiaaaaa/geocluster/main/geocluster/data/plr_id.geojson')
+df =  pd.read_csv('https://raw.githubusercontent.com/Safiaaaaa/geocluster/main/geocluster/data/full_dataset.csv')
 
 def feature_df(df, features):
     df_selected = df[features]
     df_scaled = pd.DataFrame(
         minmax_scale(df_selected), columns=features)
+    df_scaled['PLR_NAME'] = df.PLR_NAME
     df_scaled.index = df.PLR_ID
     return df_scaled
 
 def kmeans_clustering(n_clusters, df):
     kmeans = KMeans(n_clusters=n_clusters, random_state=42)
-    clusters = kmeans.fit(df)
+    clusters = kmeans.fit(df.drop(columns='PLR_NAME'))
     df["clusters"] = clusters.labels_
     return df
 
@@ -34,7 +35,6 @@ def cluster(df, n_clusters, features=['child_pov']):
     selected = feature_df(df, features)
     clusters_df = kmeans_clustering(n_clusters, selected)
     return clusters_df
-
 
 feat_labels = ['Child poverty in % per planning area',
  'Unemployment in % per planning area',
@@ -54,7 +54,7 @@ feat_labels = ['Child poverty in % per planning area',
  'Amount of public transport stops within 500 m, incl. bus per planning area',
  'Amount of restaurants and cafés within 500 m (exc. fast food) per planning area',
  'Amount of cultural institutions within 500 m (museums, cinemas, theaters, etc.) per planning area',
- #"Amount of extra-curriculum educational institutions within 500 m (music and language schools) per planning area",
+ "Amount of extra-curriculum educational institutions within 500 m (music and language schools) per planning area",
  'Amount of urban furniture (picnic tables, benches, bbq, water points, etc.) within 500 m per planning area',
  'Amount of places for outdoor leisure (swimming pools, parks, playgrounds, etc.) within 500 m per planning area',
  'Amount of bars, pubs, nightclubs, etc. within 500 m per planning area',
@@ -76,7 +76,6 @@ feat_labels = ['Child poverty in % per planning area',
  'Population with Post-Soviet states-origin in % per planning area', 'Population with Turkey origin in % per planning area',
  'Population with Arab-origin in % per planning area', 'Population with Other-origin in % per planning area',
  'Population with not identified origin in % per planning area']
-
 
 feat_dict = {'Population with migration background in % per planning area': 'mig_rate',
  'Unemployment in % per planning area': 'unemployme',
@@ -123,64 +122,75 @@ feat_dict = {'Population with migration background in % per planning area': 'mig
  'Population with Other-origin in % per planning area': 'HK_Sonst',
  'Population with not identified origin in % per planning area': 'HK_NZOrd'}
 
+# Project introduction
+st.title('Browse and cluster Berlin data')
+st.write('For our final project of the data science bootcamp at le Wagon, in September 2022, we collected 105 features from Berlin Open Data Platform and OpenStreetMaps. With this webapp, we aim at making this data available - and visible. We are using KMeans Clustering to plot the distribution of individual ans multiple features.')
 
+# Collect user input
 n_clusters = st.slider('Number of clusters', 2, 20)
 selection = st.multiselect('Features to cluster on',
      feat_labels, default=['Child poverty in % per planning area'])
 if len(selection) == 0: 
-    '''Choose the features you want to cluster on'''
-    
-# st.map(pd.DataFrame({'lat': [52.52437], 'lon': [13.41053]}), zoom=10)
+    st.header('Choose one or several features you want to cluster on')
+else: 
+        
+    features_dict = feat_dict
+    features = [features_dict[s] for s in selection]
 
-# m = folium.Map(location=[52.52437, 13.41053], zoom_start=10)
-# folium_static(m)
+    # API call
 
-features_dict = feat_dict
-features = []
-for s in selection: 
-    features.append(features_dict[s])
+    # url = 'http://127.0.0.1:8000/cluster'
+    # params={'n_clusters':n_cluster, 
+    #     'features': features
+    # }
+    # resp = requests.get(url=url, params=params).json()
 
-# API call
-# url = 'http://127.0.0.1:8000/cluster'
-# params={'n_clusters':n_cluster, 
-#     'features': features
-# }
-# resp = requests.get(url=url, params=params).json()
+    clusters_df = cluster(df, n_clusters, features)
 
-geo = gpd.read_file('https://raw.githubusercontent.com/Safiaaaaa/geocluster/main/geocluster/data/plr_id.geojson')
-df =  pd.read_csv('https://raw.githubusercontent.com/Safiaaaaa/geocluster/main/geocluster/data/full_dataset.csv')
+    # feature = 'child_pov'
+    df['cluster'] = df['PLR_ID'].map(clusters_df['clusters']).astype(int).astype(str)
 
-clusters_df = cluster(df, n_clusters, features)
+    # colors = dict.fromkeys(np.arange(1, n_cluster+1), list(np.random.choice(range(256), size=3)))
+    # f'''{df['cluster']}'''
+    labels_dict = {'PLR_ID': False}
+    for f in features: 
+        labels_dict[f] = True
 
-# feature = 'child_pov'
-df['cluster'] = df['PLR_ID'].map(clusters_df['clusters']).astype(int).astype(str)
+    fig = px.choropleth_mapbox(data_frame = df,
+            geojson=geo,
+            locations="PLR_ID",
+            featureidkey="properties.PLR_ID",
+            color='cluster',
+            color_discrete_sequence= px.colors.qualitative.Alphabet,
+            # color_discrete_map=colors,
+            # color_continuous_scale="Viridis",
+            # range_color=(df[features[selection[0]]].max(), df[features[selection[0]]].min()),
+            mapbox_style="carto-positron",
+            zoom=9,
+            center={
+                "lat": 52.52,
+                "lon": 13.40
+            },
+            opacity=0.5,
+            #labels= {f"{color}: {color} amount"},
+            hover_name='PLR_NAME',
+            hover_data=labels_dict
+            )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, legend=dict(
+    orientation="h",
+    yanchor="bottom",
+    y=1.02,
+    xanchor="right",
+    x=1
+))
+    # fig.update_traces(
+    #     hovertemplate="<br>".join(key+f': {df[key]}' for key in labels_dict.keys())
+    #     )
+  
+    st.plotly_chart(fig)
 
-# colors = dict.fromkeys(np.arange(1, n_cluster+1), list(np.random.choice(range(256), size=3)))
-# f'''{df['cluster']}'''
-
-fig = px.choropleth_mapbox(data_frame = df,
-        geojson=geo,
-        locations="PLR_ID",
-        featureidkey="properties.PLR_ID",
-        color='cluster',
-        color_discrete_sequence= px.colors.qualitative.Alphabet,
-        # color_discrete_map=colors,
-        # color_continuous_scale="Viridis",
-        # range_color=(df[features[selection[0]]].max(), df[features[selection[0]]].min()),
-        mapbox_style="carto-positron",
-        zoom=9,
-        center={
-            "lat": 52.52,
-            "lon": 13.40
-        },
-        opacity=0.5,
-        #labels= {f"{color}: {color} amount"},
-        hover_name='PLR_NAME',
-        hover_data=features
-        )
-
-cluster_centers = df.groupby('cluster').mean()[features]
-st.dataframe(cluster_centers)
-# fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},) # coloraxis_colorbar= {'title':f"{shortname} in {unit}"}
-st.plotly_chart(fig)
-st.write("")
+    with st.container():
+        st.subheader("Mean values of selected features per cluster")
+        cluster_centers = df.groupby('cluster').mean()[features]
+        st.dataframe(cluster_centers, use_container_width=True)
+        # fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},) # coloraxis_colorbar= {'title':f"{shortname} in {unit}"}
